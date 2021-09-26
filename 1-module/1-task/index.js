9976514debf09ef6013edb7945eab9903349818c
m.panin <m.kovalinskij@ecomilk.ru>
2021-09-26 23:10:57 +0300

    1 module\1-task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,26 @@
 function factorial(n) {
-  // ваш код...
+    let result = 1;
+    if (n === 0 || n === 1) {
+        return result;
+    }
+
+    for (let i = 1; i <= n; i++) {
+        result = result * i;
+    }
+
+    return result;
 }
+
+
+
+
+// Подскажите пожалуйста, почему данная ниже функция не прошла тест?
+/*
+function factorial(n){
+  let result = 1;
+  while(n){
+      result *= n--;
+  }
+  return result;
+}
+*/
